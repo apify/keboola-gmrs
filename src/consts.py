129b32ff89f3a97ad194654053748c9f21dce391
oklinov <oklinov@tutanota.com>
2025-04-07 17:35:15 +0200

    imprv: store dataset fields to state.json
    - component will store dataset fields to state.json and extend it with
      any new one from subsequent runs
    - dataset items are downloaded in csv format so we don't need to handle
      field flattening
    - remove OUTPUT_COLUMNS

--- src/consts.py
+++ src/consts.py
@@ -1,37 +1,3 @@
-OUTPUT_COLUMNS = [
-    'reviewerId',
-    'reviewerUrl',
-    'name',
-    'reviewerNumberOfReviews',
-    'isLocalGuide',
-    'reviewerPhotoUrl',
-    'reviewId',
-    'text',
-    'originalLanguage',
-    'textTranslated',
-    'translatedLanguage',
-    'publishAt',
-    'publishedAtDate',
-    'likesCount',
-    'reviewUrl',
-    'reviewOrigin',
-    'responseFromOwnerDate',
-    'responseFromOwnerText',
-    'reviewImageUrls',
-    'reviewContext',
-    'reviewDetailedRating',
-    'visitedIn',
-    'placeId',
-    'title',
-    'stars',
-    'rating',
-    'totalScore',
-    'url',
-    'cid',
-    'fid',
-    'searchString',
-]
-
 ACTOR_ID = 'Xb8osYTtOjlsgI6k9'
 
 REQUIRED_PARAMETERS = [
